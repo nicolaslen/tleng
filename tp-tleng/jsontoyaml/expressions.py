e0bfbedee34a2c5aca9993aff6f37b390495a4fb
Nicolas Len <nicolaslen@gmail.com>
2018-07-03 13:27:29 -0300

    a

--- tp-tleng/jsontoyaml/expressions.py
+++ tp-tleng/jsontoyaml/expressions.py
@@ -6,6 +6,16 @@ class Expression(object):
   def value(self, prefixs):
     raise NotImplementedError("Este método lo deben implementar las clases que hereden de Expression")
 
+  def indent(self, prefixs, exp):
+    spaces = ""
+    prefix = ""
+    
+    if prefixs:
+      spaces = ("  " * (len(prefixs) - 1))
+      prefix = prefixs[len(prefixs) - 1]
+
+    return "{0}{1}{2}".format(spaces, prefix, exp)
+
 class ValueExpression(Expression):
   def __init__(self, expression):
     self.expression = expression
@@ -18,73 +28,70 @@ class ValuePopExpression(Expression):
     self.expression = expression
 
   def value(self, prefixs):
-    exp = "{0}{1}".format("\n", self.expression.value(prefixs))
+    exp = self.expression.value(prefixs)
     prefixs.pop()
     return exp
     
+# O -> { M }
 class ObjectExpression(Expression):
   def __init__(self, members):
     self.members = members
 
   def value(self, prefixs):
     prefixs.append("")
-    return self.members.value(prefixs)
+    return "\n{0}".format(self.members.value(prefixs)) 
 
+# O -> { }
 class ObjectEmptyExpression(Expression):
   def value(self, prefixs):
     prefixs.append("")
     return "{}"
 
+# A -> [ ]
 class ArrayEmptyExpression(Expression):
   def value(self, prefixs):
     prefixs.append("- ")
     return "[]"
 
+# A -> [ E ]
 class ArrayExpression(Expression):
   def __init__(self, expression):
     self.expression = expression
 
   def value(self, prefixs):
     prefixs.append("- ")
-    return self.expression.value(prefixs)
-  
+    return "\n{0}".format(self.expression.value(prefixs))
+
+# E -> V , E
+# M -> P , M
 class ElementListExpression(Expression):
   def __init__(self, valueExpression, elementsExpression):
     self.valueExpression = valueExpression
     self.elementsExpression = elementsExpression
 
   def value(self, prefixs):
-    spaces = ""
-    prefix = ""
-    
-    if prefixs:
-      spaces = ("  " * (len(prefixs) - 1))
-      prefix = prefixs[len(prefixs) - 1]
+    exp = "{0}\n{1}".format(self.valueExpression.value(prefixs), self.elementsExpression.value(prefixs))
+    return Expression.indent(self, prefixs, exp)
 
-    return "{0}{1}{2}{3}{4}".format(spaces, prefix, self.valueExpression.value(prefixs), "\n", self.elementsExpression.value(prefixs))
-
+# M -> P
+# E -> V
 class ElementValueExpression(Expression):
   def __init__(self, expression):
     self.expression = expression
 
   def value(self, prefixs):
-    spaces = ""
-    prefix = ""
-    
-    if prefixs:
-      spaces = ("  " * (len(prefixs) - 1))
-      prefix = prefixs[len(prefixs) - 1]
+    return Expression.indent(self, prefixs, self.expression.value(prefixs))
 
-    return "{0}{1}{2}".format(spaces, prefix, self.expression.value(prefixs))
-
+# M -> S : V
 class PairExpression(Expression):
   def __init__(self, stringExpression, valueExpression):
     self.stringExpression = stringExpression
     self.valueExpression = valueExpression
 
   def value(self, prefixs):
-    return "{0}{1}{2}".format(self.stringExpression.value(prefixs), ": ", self.valueExpression.value(prefixs))
+    return "{0}: {1}".format(self.stringExpression.value(prefixs), self.valueExpression.value(prefixs))
 
+# S -> " string "
 class StringExpression(Expression):
   def __init__(self, expression):
     self.expression = expression
@@ -92,6 +99,7 @@ class StringExpression(Expression):
   def value(self, prefixs):
     return self.expression
 
+# V -> num
 class NumberExpression(Expression):
   def __init__(self, expression):
     self.expression = expression
@@ -99,6 +107,7 @@ class NumberExpression(Expression):
   def value(self, prefixs):
     return str(self.expression)
 
+# V -> true
 class TrueExpression(Expression):
   def __init__(self, expression):
     self.expression = expression
@@ -106,6 +115,7 @@ class TrueExpression(Expression):
   def value(self, prefixs):
     return "true"
 
+# V -> false
 class FalseExpression(Expression):
   def __init__(self, expression):
     self.expression = expression
@@ -113,6 +123,7 @@ class FalseExpression(Expression):
   def value(self, prefixs):
     return "false"
 
+# V -> null
 class NullExpression(Expression):
   def __init__(self, expression):
     self.expression = expression
